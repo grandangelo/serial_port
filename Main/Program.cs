e70ffdbc9bad6abe260c791db1f15a34c69bacc8
grandangelo <grandangelo@zoho.com>
2024-09-27 15:06:44 +0200

    Add files

---
+++ Main/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+try
+{
+    var serialPortPath = "..\\..\\..\\..\\SerialPort\\bin\\Debug\\net8.0-windows\\SerialPort.dll";
+    var serialAssembly = Assembly.LoadFrom(serialPortPath);
+    var serialPort = Activator.CreateInstance(serialAssembly.GetType("SerialPort.SerialPort"));
+}
+catch
+{
+    // Do nothing
+}
+
+
---
+++ SerialPort/SerialPort.cs
@@ -0,0 +1,10 @@
+﻿namespace SerialPort
+{
+    public class SerialPort
+    {
+        public SerialPort()
+        {
+            var serial = new System.IO.Ports.SerialPort();
+        }
+    }
+}
